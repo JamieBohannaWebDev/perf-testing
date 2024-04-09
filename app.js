db09b823e127d80b8bafbbf451d02a806fc651bc
Jamie Bohanna <jamie.bohanna@servita.com>
2024-04-09 18:43:33 +0100

    Init

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+app.get('/status', (req, res) => {
+    const status = {
+        "Status": "Running",
+    };
+
+    res.status(200).json(status);
+});
